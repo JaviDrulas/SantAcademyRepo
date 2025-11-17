0417d85a5923fc10c3fb309279ac8150eca221fd
Oliwia  Wysokinska <EM2025008552@nfoque.local>
2025-11-17 14:26:31 +0100

    Added Oliwia.package

---
+++ Oliwia.package/start_nfqv16_2024.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Mykola change his script
+echo Inicializando entorno...
+sleep 2s
+
+echo Inicializando Event Server
+nohup ./eventServer.sh &
+sleep 30s
+
+echo Inicializando Data Server
+nohup ./dataServer.sh &
+sleep 30s
+
+echo Inicializando Engine Server
+nohup ./engineServer.sh &
+sleep 30s
+
+echo Inicializando Risk Server
+nohup ./riskServer.sh &
+sleep 30s
+
+echo Inicializando webstart
+nohup ./webstart.sh &
+sleep 10s
+
+echo Inicializando scheduler
+nohup ./scheduler.sh &
+
+echo Ambiente Inicializado
